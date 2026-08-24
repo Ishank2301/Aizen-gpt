67e4e8cbb3f09b168447895fd427b95247d588d2
Ishank <ishankmishra579@gmail.com>
2026-08-25 00:38:24 +0530

    Implement Positional encoding (NLP 4/4)

---
+++ model/positional_encoding.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def get_positional_encoding(
+        self,
+        seq_len: int,
+        d_model: int
+    ) -> NDArray[np.float64]:
+
+        position = np.arange(seq_len)[:, np.newaxis]
+        div_term = 10000 ** (
+            np.arange(0, d_model, 2) / d_model
+        )
+
+        pe = np.zeros((seq_len, d_model))
+
+        pe[:, 0::2] = np.sin(position / div_term)
+        pe[:, 1::2] = np.cos(position / div_term)
+
+        return np.round(pe, 5)
